46e348e27a05ef19c6461ca7f90de94f2e79a10a
Ankush2107 <ankush.kumar210701@gmail.com>
2023-08-14 20:40:39 +0530

    Inheritance lecture 1

---
+++ Inheritance/Demo.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Demo {
+    public static void main(String[] args) {
+        AdvCalc obj = new AdvCalc();
+        int r1 = obj.add(5, 8);
+        int r2 = obj.sub(16, 8);
+        int r3 = obj.mul(5, 6);
+        System.out.println(r1 + " " + r2 + " " + r3);
+    }
+}
